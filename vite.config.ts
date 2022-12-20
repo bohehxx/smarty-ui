d7ea2ee308edb75c83bc17025242bc0d66e5581d
bohehxx <2674680690@qq.com>
2022-12-20 15:57:58 +0800

    feat(es): add es

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,25 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import vueJsx from "@vitejs/plugin-vue-jsx"
-import Unocss from "./config/unocss"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import Unocss from "./config/unocss";
 
 const rollupOptions = {
   external: ["vue", "vue-router"],
   output: {
     globals: {
-      vue: "vue",
+      vue: "Vue",
     },
   },
-}
+};
 
 export default defineConfig({
   plugins: [vue(), vueJsx({}), Unocss()],
   build: {
     rollupOptions,
-    minify: false,
+    minify: "terser",
+    sourcemap: true,
+    brotlisize: true,
     cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
@@ -27,11 +29,11 @@ export default defineConfig({
       formats: ["esm", "umd", "iife"],
     },
   },
-  test:{
-    globals:true,
-    environment:"happy-dom",
-    transformMode:{
-      web:[/.[tj]sx$/]
-    }
-  }
-})
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    transformMode: {
+      web: [/.[tj]sx$/],
+    },
+  },
+});
